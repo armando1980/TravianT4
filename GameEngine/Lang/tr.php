25b69ba19f4e3efc65e0f7f23d2218489f6f583c
unknown <Idan@Idan-Laptop.(none)>
2012-03-29 22:56:19 +0200

    first commit

---
+++ GameEngine/Lang/tr.php
@@ -0,0 +1,340 @@
+﻿<?php
+
+//TRIBES
+define("TRIBE1","Romalılar"); 
+define("TRIBE2","Cermenler");
+define("TRIBE3","Galyalılar"); 
+define("TRIBE4","Doğa"); 
+define("TRIBE5","Natarlar");
+define("TRIBE6","Monsters");
+
+
+//MAIN MENU
+define("HOME","Anasayfa");
+define("LOGIN","Giris");
+define("REG","Kayit Ol");
+define("FORUM","Forum");
+define("SUPPORT","Destek");
+define("LOGOUT","Cikis");
+define("HEADER_DORF1","Dis Taraf");
+define("HEADER_DORF2","Ic Taraf");
+define("HEADER_MAP","Harita");
+define("HEADER_STATS","Istatistikler");
+define("HEADER_NOTICES","Raporlar");
+define("HEADER_NOTICES_NEW","Yen' Rapor");
+define("HEADER_MESSAGES","Mesajlar");
+define("HEADER_MESSAGES_NEW","Yeni Mesaj");
+
+
+//LOGIN.php
+define("LOGIN_WELCOME","Hosgeldiniz ".SERVER_NAME."");
+define("LOGIN_NO_JAVASCRIPT","JavaScript JavaScript");
+define("LOGIN_USERNAME","Kullanici Adi");
+define("LOGIN_PASSWORD","Sifre");
+define("LOGIN_LOWRES_OPTION","Oyuncu Duzenleme");
+define("LOGIN_LOWRES_DESC","");
+define("LOGIN_LOWRES_NOTICE","");
+define("LOGIN_PW_FORGOTTEN","Sifremi Unuttum");
+define("LOGIN_PW_EMAIL","E-Posta:");
+define("LOGIN_PW_BTN","");
+define("LOGIN_PW_REQUEST","Yeni Bir Sifre Gonderecegiz.");
+define("LOGIN_PW_SENT","E-postaniza Yeni Sifrenizi Iceren Bir Mesaj Yolladik");
+define("LOGIN_SERVER_START","Oyun Henuz Baslamamistir");
+define("LOGIN_SERVER_START2","Sen.");
+define("PW_GENERATE","Tum Alanlari Doldurun");
+define("EMAIL_NOT_VERIFIED","E-Posta Dogrulanamadi");
+define("EMAIL_FOLLOW","Hesabinizi Etkinlestirmek Icin Bu Baglantiya Tiklayin");
+define("VERIFY_EMAIL","E-Posta Onaylandi");
+define("COOKIES","");
+
+
+//LOGOUT.php
+define("LOGOUT_TITLE","Cikis Islemi Basariyla Gerceklesti");
+define("LOGOUT_H4","Oyuna Girdiginiz Icin Tesekkurler.");
+define("LOGOUT_DESC","");
+define("LOGOUT_LINK","");
+
+
+//REGISTER
+define("REGISTER_USERINFO","Kullanici Bilgileri");
+define("REGISTER_USERNAME","Kullanici Adi :");
+define("REGISTER_EMAIL","E-Posta :");
+define("REGISTER_LOCATION","Bolgenizi Secin :");
+define("REGISTER_RANDOM","Rasgele");
+define("REGISTER_NW","Kuzey Bati");
+define("REGISTER_NE","Kuzey Dogu");
+define("REGISTER_SW","Guney Bati");
+define("REGISTER_SE","Guney Dogu");
+define("ACCEPT_RULES"," Kurallari Okudum Ve Kabul Ediyorum.");
+define("REGISTER_MOREINFO","Daha Fazla Bilgi");
+define("REGISTER_PASSWORD","Sifre :");
+define("REGISTER_SELECT_TRIBE","Halkinizi Secin ");
+define("BEFORE_REGISTER","");
+define("REGISTER_STARTER","");
+
+
+//CONTACT.PHP
+define("CONTACT_DESC","Buradan Mesaj Gonderebilirsiniz");
+define("CONTACT_CATEGORY","Katagori");
+define("CONTACT_SELECT_SUBJECT","Lutfen Secin");
+define("CONTACT_SUBJECT1","Oyun Ici");
+define("CONTACT_SUBJECT2","Hata Bug Bildirimi");
+define("CONTACT_SUBJECT3","Altin Satin Alma Hakkinda");
+define("CONTACT_SUPPORT","Destek");
+define("CONTACT_USERNAME","Kullanici Adiniz");
+define("CONTACT_USER_NOT_EXIST","Kullanici Adi Kayitli Degil .");
+define("CONTACT_EMAIL","E-Posta");
+define("CONTACT_MESSAGE","Mesajiniz");
+define("CONTACT_SEND","Gonder");
+define("CONTACT_SENT","Basariyla Gonderildi.");
+
+
+//ERRORS
+define("USRNM_EMPTY","(Kullanici Adi Bos)");
+define("USRNM_TAKEN","(Kullanici Adi Kullaniliyor)");
+define("USRNM_SHORT","(".USRNM_MIN_LENGTH.")");
+define("USRNM_CHAR","(Gecersiz Karakterler)");
+define("PW_EMPTY","(Sifre Bos)");
+define("PW_SHORT","(".PW_MIN_LENGTH.")");
+define("PW_INSECURE","(Guvensiz Bir Sifre Daha Guvenli Bir Sifre Girin)");
+define("EMAIL_EMPTY","(E-Posta Bos)");
+define("EMAIL_INVALID","(Gecersiz E-Mail Adresi)");
+define("EMAIL_NOTEXIST","??? ????? ???? ?????.");
+define("EMAIL_TAKEN","(E-Posta Zaten Kullaniliyor)");
+define("TRIBE_EMPTY","<li>Bir Kabile Secin</li>");
+define("AGREE_ERROR","<li>Kaydolmak Icin Oyun Sartlarini Kabul Etmek Zorundasiniz</li>");
+define("LOGIN_USR_EMPTY","Isim Girin");
+define("LOGIN_PASS_EMPTY","Sifre Girin");
+define("EMAIL_ERROR","Girdiginiz E-Posta Adresi Gecerli Degil");
+define("PASS_MISMATCH","Sifreler Uyusmuyor");
+define("ALLI_OWNER","Bir Halk Secin");
+define("SIT_ERROR","");
+define("USR_NT_FOUND","Adiniz Bulunamadi");
+define("USR_BAN","Hesabiniz Engellendi");
+define("LOGIN_PW_ERROR","Sifre Yanlis");
+define("WEL_TOPIC","Aciklama Bilgi");
+define("ATAG_EMPTY","Sembol Bos");
+define("ANAME_EMPTY","Ad Bos");
+define("ATAG_EXIST","Zaten Kayitli");
+define("ANAME_EXIST","Zaten Kayitli");
+
+
+//BUILD.TPL
+define("CUR_PROD","Mevcut Uretim");
+define("NEXT_PROD","Uretim Seviyesi");
+define("BUILDING_UPGRADING","Seviye:");
+define("HOURS","Zaman");
+
+
+//WARSIM.TPL
+define("WARSIM","Savas Smilatoru");
+define("WARSIM_ATTACKER","Saldiran");
+define("WARSIM_DEFENDER","Koruyan");
+define("WARSIM_ETC","Diger");
+
+define("WARSIM_POP","Nufus");
+define("WARSIM_KATA","Mancinik Hedef Seviyesi");
+define("WARSIM_HEROPOWER","Kahraman Saldiri Puani");
+define("WARSIM_STONEMASON","Duvar");
+define("WARSIM_PALACE","Saray \ Kosk");
+
+//BUILDINGS
+define("B1","Orman");
+define("B1_DESC","Oduncu ormanlarda ağaç keserek köyünün kereste ihtiyaçlarını karşılar. Seviyesinin yüksekliğine göre daha fazla odun keser.");
+define("B2","Tuğla Ocağı");
+define("B2_DESC","Tuğla ocaklarında tuğla üretilir. Her tuğla ocağı seviyesinin yüksekliğine göre tuğla üretir. Seviye artırıldıkça üretimde artar.");
+define("B3","Demir Madeni");
+define("B3_DESC","Demir madenlerinden demir madeni çıkarılır. Seviye artırıldıkça çıkarılan demir miktarıda artar.");
+define("B4","Tarla");
+define("B4_DESC","Tarlalarda köy halkı için tahıl yetiştirilir. Tahıl üretimi çiftliğin seviyesi arttıkça artar.");
+
+//DORF1
+define("LUMBER","Odun");
+define("CLAY","Tuğla");
+define("IRON","Demir");
+define("CROP","Tahıl");
+define("LEVEL","Seviye");
+define("CROP_COM",CROP." Tüketimi");
+define("PER_HR","adet");
+define("PROD_HEADER","Saatlik Üretim");
+define("MULTI_V_HEADER","Köyler"); 
+define("ANNOUNCEMENT","Announcement");
+define("GO2MY_VILLAGE","Go to my village");
+define("VILLAGE_CENTER","Village centre");
+define("FINISH_GOLD","Finish all construction and research orders in this village immediately for 2 Gold?");
+define("WAITING_LOOP","(işlem sırası)");
+define("HRS","sürer");
+define("DONE_AT","bu saatte hazır");
+define("CANCEL","cancel");
+define("LOYALTY","Vatan Sevgisi:"); 
+define("CALCULATED_IN","Sayfayı yükleme zamanı");
+define("SEVER_TIME","Server saati:");  
+define("SIDEINFO_CHANGEVIL_BTN","Market"); 
+define("SIDEINFO_CHANGEVIL_LABEL","Koyun Yeni Adi:"); 
+define("SIDEINFO_CHANGEVIL_TITLE","Koy Isim Degisikligi"); 
+define("SIDEINFO_CHANGE_TITLE","Tekrar Isim Degistirmek Icin Kutuya Cift Tiklayin."); 
+define("SIDEINFO_ADVENTURES","Macera"); 
+define("SIDEINFO_AUCTIONS","Madde"); 
+define("SIDEINFO_PROFILE","Profil");
+define("SIDEINFO_ALLIANCE","Birlik");
+define("SIDEINFO_ALLY_FORUM","???? ?????");
+
+
+//======================================================//
+//================ UNITS - DO NOT EDIT! ================//
+//======================================================//
+
+//HERO
+define("U0","Kahraman");
+
+//ROMA BİRİMLERİ
+define("U1","Lejyoner");
+define("U2","Pretoryan");
+define("U3","Emperyan");
+define("U4","Equites Legati");
+define("U5","Equites Imperatoris");
+define("U6","Equites Caesaris");
+define("U7","Koçbaşı");
+define("U8","Ateş Mancınığı");
+define("U9","Senatör");
+define("U10","Göçmen");
+
+//CERMEN BİRİMLERİ
+define("U11","Tokmak Sallayan");
+define("U12","Mızrakçı");
+define("U13","Balta Sallayan");
+define("U14","Casus");
+define("U15","Paladin");
+define("U16","Toyton");
+define("U17","Koçbaşı");
+define("U18","Mancınık");
+define("U19","Reis");
+define("U20","Göçmen");
+
+//GALYA BİRİMLERİ
+define("U21","Phalanx");
+define("U22","Kılıçlı");
+define("U23","Gözetçi");
+define("U24","Toytatın Şimşeği");
+define("U25","Druyid");
+define("U26","Heduan");
+define("U27","Koçbaşı");
+define("U28","Mancınık");
+define("U29","Kabile Reisi");
+define("U30","Göçmen");
+
+//DOĞA BİRİMLERİ
+define("U31","Sıçan");
+define("U32","Örümcek");
+define("U33","Yılan");
+define("U34","Yarasa");
+define("U35","Yaban Domuzu");
+define("U36","Kurt");
+define("U37","Ayı");
+define("U38","Timsah");
+define("U39","Kaplan");
+define("U40","Fil");
+
+//NATAR BİRİMLERİ
+define("U41","Sipahi");
+define("U42","Thorned Warrior");
+define("U43","Guardsman");
+define("U44","Natar kartalı");
+define("U45","Axerider");
+define("U46","Natarian Knight");
+define("U47","Savaş Fili");
+define("U48","Ballista");
+define("U49","Natar İmparatoru");
+define("U50","Göçmen");
+
+//INDEX.php
+define("LOGIN","Giriş Yap");
+define("PLAYERS","Oyuncular");
+define("ONLINE","Aktif");
+define("TUTORIAL","Oyun Turu");
+define("PLAYER_STATISTICS","Oyuncu İstatistikleri");
+define("TOTAL_PLAYERS","Kayıtlı Oyuncu");
+define("ACTIVE_PLAYERS","Aktif Oyuncu");
+define("ONLINE_PLAYERS","Çevrimiçi Oyuncu");
+define("MP_STRATEGY_GAME","TRAVIAN - Tarayıcı Tabanlı Devasa Oyun");
+define("WHAT_IS","TRAVIAN, en başarılı tarayıcı tabanlı oyunlardan bir tanesi. TRAVIAN'da bir oyuncu olarak, bir çok köyler kurabilir, büyük savaşlara katılabilir, büyük bir birliğin parçası olarak diğer oyunculara meydan okuyabilirsiniz.");
+define("REGISTER_FOR_FREE","Hemen ücretsiz kayıt ol!");
+define("LATEST_GAME_WORLD","Son oyun dünyası");
+define("LATEST_GAME_WORLD2","En son oyun dünyasına kayıt ol ve<br> Travian oynamanın tadını çıkar. <br/>Üstelik ilk oyuncularla!");
+define("PLAY_NOW","Şimdi ".SERVER_NAME." oyna");
+define("LEARN_MORE","Hakkında daha fazla bilgi:<br/> ".SERVER_NAME."!");
+define("COMUNITY","Topluluk");
+define("NEWS","Haberler");
+define("SCREENSHOTS","Screenshots");
+define("LOGIN_TO","Log in to ". SERVER_NAME);
+define("REGIN_TO","Register in ". SERVER_NAME);
+define("P_ONLINE","Çevrimiçi Oyuncu: ");
+define("P_TOTAL","Kayıtlı Oyuncu: ");
+define("CHOOSE","Please choose a server.");
+define("STARTED"," Serverda oyun ". round((time()-COMMENCE)/86400) ." gün önce başladı.");
+
+
+//ATTACKS ETC.
+define("TROOP_MOVEMENTS","Askeri Hareket:"); 
+define("ARRIVING_REINF_TROOPS","Destek yolda");
+define("ARRIVING_REINF_TROOPS_SHORT","Destek");
+define("OWN_ATTACKING_TROOPS","Saldırıdaki Askerler");
+define("ATTACK","Saldırı");
+define("OWN_REINFORCING_TROOPS","Desteklenen Askerler");
+define("TROOPS_DORF","Askerler:");
+
+
+//404.php
+define("NOTHING_HERE","Burada Yok!");
+define("WE_LOOKED","404 kez aradık, fakat hiçbirşey bulamadık!");
+
+//HEADER.TPL
+define("SERVER_TIME","???? ????:");
+define("HEADER_DAY","???");
+define("HEADER_NIGHT","??");
+define("HEADER_GOLD","??? ???? ??????");
+define("HEADER_SILVER","??? ???? ??????");
+define("HEADER_PLUS","????");
+define("HEADER_PLUSMENU","???? ????");
+define("HEADER_ADMIN","??????");
+
+//NOTICES
+define("REPORT_SUBJECT","Baslik:");
+define("REPORT_ATTACKER","Saldiran");
+define("REPORT_DEFENDER","Savunan");
+define("REPORT_RESOURCES","Hammadde");
+define("REPORT_FROM_VIL","Köy");
+define("REPORT_FROM_ALLY","Birlik");
+define("REPORT_SENT","Gonderilen");
+define("REPORT_SENDER","Gonderen");
+define("REPORT_RECEIVER","Alici");
+define("REPORT_AT","??");
+define("REPORT_TO","??");
+define("REPORT_SEND_RES","Hammadde Gonderir");
+define("REPORT_DEL_BTN","Kaldirildi");
+define("REPORT_DEL_QST","");
+define("REPORT_WARSIM","Simulator Ile Mucadele");
+define("REPORT_ATK_AGAIN","Tekrar Saldir");
+define("REPORT_TROOPS","Askerler");
+define("REPORT_REINF","Yardim");
+define("REPORT_CASUALTIES","Kayiplar");
+define("REPORT_INFORMATION","Bilgi");
+define("REPORT_BOUNTY","Ganimet");
+define("REPORT_CLOCK","Zaman");
+define("REPORT_UPKEEP","Hammadde");
+define("REPORT_PER_HOURS","Saat Basina");
+define("REPORT_SEND_REINF_TO","Yardimci Kuvvet Gonderildi");
+
+//MASSMESSAGE.php
+define("MASS","Message Content");
+define("MASS_SUBJECT","Subject:");
+define("MASS_COLOR","Message color:");
+define("MASS_REQUIRED","All fields required");
+define("MASS_UNITS","Images (units):");
+define("MASS_SHOWHIDE","Show/Hide");
+define("MASS_READ","Read this: after adding smilie, you have to add left or right after number otherwise image will won't work");
+define("MASS_CONFIRM","Confirmation");
+define("MASS_REALLY","Do you really want to send MassIGM?");
+define("MASS_ABORT","Aborting right now");
+define("MASS_SENT","Mass IGM was sent");
+?>
